229cf292ce49a2b99510d20342a5b1a3944e4ffd
lucas <lucasmengue96@hotmail.com>
2022-03-25 22:32:41 -0300

    activated and using the visibilityjs

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,31 @@ $(".rslides_portfolio").responsiveSlides({
   timeout: 4000, // Integer: Time between slide transitions, in milliseconds
   pager: true, // Boolean: Show pager, true or false
 });
+
+/* Ativando o Visibilityjs */
+Visibility.onVisible(function () {
+  /* Animações da home */
+  setTimeout(function () {
+    $(".introducao h1").addClass("animate__animated animate__fadeInDown");
+  }, 400);
+  setTimeout(function () {
+    $(".introducao blockquote").addClass("animate__animated animate__fadeInDown");
+  }, 800);
+  setTimeout(function () {
+    $(".introducao .btn").addClass("animate__animated animate__fadeInDown");
+  }, 1200);
+  setTimeout(function () {
+    $(".animar").addClass("animate__animated animate__fadeInDown");
+  }, 1600);
+
+  /* Animações das páginas internas */
+  setTimeout(function () {
+    $(".introducao-interna h1").addClass("animate__animated animate__fadeInDown");
+  }, 400);
+  setTimeout(function () {
+    $(".introducao-interna p").addClass("animate__animated animate__fadeInDown");
+  }, 800);
+  setTimeout(function () {
+    $(".animar-interno").addClass("animate__animated animate__fadeInDown");
+  }, 1200);
+});
